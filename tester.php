6abcbae4303e855c6bba8132ae927b92367a55e8
antoakun <antokarjeun@gmail.com>
2017-11-10 12:51:24 +0700

    Memasukan HTML

---
+++ tester.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<tabel>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+	</tabel>
+</body>
+</html>
